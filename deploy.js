5e1bddde6b77ddf3e06ec6b5b7d7bfd4be0a4745
Christopher Raines <christopher.raines@jessup.edu>
2022-06-27 13:17:32 -0400

    applied suggested fix

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.234.214.97',
+      host: '3.80.74.70',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
